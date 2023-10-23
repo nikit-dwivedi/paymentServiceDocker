f0552779bbe6e34adb83b04d1f4c68b82272ae22
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-10-23 14:33:54 +0530

    deploent script

--- app/index.js
+++ app/index.js
@@ -5,15 +5,15 @@ const https = require('https');
 const http = require('http')
 
 
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
-let credentials = {key: privateKey, cert: certificate};
+let credentials = { key: privateKey, cert: certificate };
 
 const server = http.createServer(app)
-const port = process.env.APPID
+const port = process.env.APPID || 7000
 
-const secureServer = https.createServer(credentials,app);
+const secureServer = https.createServer(credentials, app);
 
 secureServer.listen(7008, () => {
     console.log(`Secure server listening at 7008`);
